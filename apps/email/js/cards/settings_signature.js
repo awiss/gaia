50ac51e0d0d9c1e174108d48fcdd4627b71521d8
Alex Wissmann <awiss@wharton.upenn.edu>
2014-07-21 18:43:04 -0700

    changed signature input from textarea to contenteditable div

--- apps/email/js/cards/settings_signature.js
+++ apps/email/js/cards/settings_signature.js
@@ -19,18 +19,19 @@ function SettingsSignatureCard(domNode, mode, args) {
   domNode.getElementsByClassName('tng-signature-done')[0]
     .addEventListener('click', this.onClickDone.bind(this), false);
 
-  this.signatureNode.value = this.identity.signature;
+  this.populateEditor(this.identity.signature);
 
 }
 
 SettingsSignatureCard.prototype = {
 
   onBack: function() {
+    console.log('leaving identity', this.identity.signature);
     Cards.removeCardAndSuccessors(this.domNode, 'animate', 1);
   },
 
   onClickDone: function() {
-    var signature = this.signatureNode.value;
+    var signature = this.fromEditor();
 
     // Only push the signature if it was changed
     if (signature !== this.identity.signature) {
@@ -41,6 +42,45 @@ SettingsSignatureCard.prototype = {
   },
 
   die: function() {
+  },
+
+  /**
+   * Inserts text into the contenteditable div
+   */
+  populateEditor: function(value) {
+    var lines = value.split('\n');
+    var frag = document.createDocumentFragment();
+    for (var i = 0, len = lines.length; i < len; i++) {
+      if (i) {
+        frag.appendChild(document.createElement('br'));
+      }
+      frag.appendChild(document.createTextNode(lines[i]));
+    }
+    this.signatureNode.appendChild(frag);
+  },
+
+  /**
+   * Gets the raw value from the contenteditable div
+   */
+  fromEditor: function() {
+    var content = '';
+    var len = this.signatureNode.childNodes.length;
+    for (var i = 0; i < len; i++) {
+      var node = this.signatureNode.childNodes[i];
+      if (node.nodeName === 'BR' &&
+          // Gecko's contenteditable implementation likes to create a synthetic
+          // trailing BR with type="_moz".  We do not like/need this synthetic
+          // BR, so we filter it out.  Check out
+          // nsTextEditRules::CreateTrailingBRIfNeeded to find out where it
+          // comes from.
+          node.getAttribute('type') !== '_moz') {
+        content += '\n';
+      } else {
+        content += node.textContent;
+      }
+    }
+
+    return content;
   }
 };
 
